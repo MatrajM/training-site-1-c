3de6dcb126cdc97ab155812c198593d28b2f4a74
Michał Matraj <matraj.michal@op.pl>
2019-07-19 19:29:20 +0200

    step five

--- index.js
+++ index.js
@@ -37,3 +37,18 @@ for (var i = 0; i < span.length; i++) {
         modal2.style.display = "none";
     }, false);
   }
+
+  function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  document.getElementById("homeDefault").click();
